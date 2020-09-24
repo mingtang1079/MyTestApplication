5dbfcb1ad525dfb659d8fba10c9ff3cc82beb873
tangming <tangming@medlinker.com>
2020-09-24 16:28:40 +0800

    test

--- app/src/main/java/com/example/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace wifsdsfsfth your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace wifsdsfsfth fdsfsdfsdyour own action", Snackbar.LENGTH_LONG)
                     .setAction("Actifsdfsdfdson", null).show()
         }
     }
